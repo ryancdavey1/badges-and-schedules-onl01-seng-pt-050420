c91dcf688ccf2cfc8ef4f556e38c87358f8f0f95
Ryan D <ryancdavey1@gmail.com>
2020-05-10 00:31:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,5 +18,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts 
+  batch_badge_creator(attendees).each {|badge| puts badge} 
+  assign_rooms(attendees).each {|assignment| puts assignment}
 end
