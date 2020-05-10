ef872fc69caaa8e34414d5b44345b5d8d5998665
Ryan D <ryancdavey1@gmail.com>
2020-05-10 00:25:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 
 def assign_rooms(attendees)
   room_assigments = []
-  attendees.each_with_index { |name, index| room_assigments << ""
+  attendees.each_with_index { |name, index| room_assigments << "Hello, #{name}! You'll be assigned to room _____!"
 }
 end
