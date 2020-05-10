3b80c8ef69220eb22913e0d616a0c77337201e02
Ryan D <ryancdavey1@gmail.com>
2020-05-10 00:17:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  badge_messages = []
+def batch_badge_creator(attendees)
+  badges = []
   names.each{|name| badge_messages << badge_maker(name)}
   badge_messages
 end
